84944ffc40a6e6f7462c2d44882c1b2d3db0a4b7
Vincenzo Favara <vin.favara@gmail.com>
2017-05-25 00:25:47 +0100

    update to swift 3

--- Source/CheckAndXHUD.swift
+++ Source/CheckAndXHUD.swift
@@ -8,51 +8,51 @@
 
 import UIKit
 
-public class CheckAndXHUD: NSObject {
-    private static let sharedObject = CheckAndXHUD()
+open class CheckAndXHUD: NSObject {
+    fileprivate static let sharedObject = CheckAndXHUD()
     let checkAndXView = CheckAndXView()
 
-    public static func showCheckInView(view: UIView) {
+    open static func showCheckInView(_ view: UIView) {
         CheckAndXHUD.sharedObject.showInView(view, message: nil, isDone: true)
     }
     
-    public static func showXInView(view: UIView) {
+    open static func showXInView(_ view: UIView) {
         CheckAndXHUD.sharedObject.showInView(view, message: nil, isDone: false)
     }
     
-    public static func showCheckInView(view: UIView, message: String?) {
+    open static func showCheckInView(_ view: UIView, message: String?) {
         CheckAndXHUD.sharedObject.showInView(view, message: message, isDone: true)
     }
     
-    public static func showXInView(view: UIView, message: String?) {
+    open static func showXInView(_ view: UIView, message: String?) {
         CheckAndXHUD.sharedObject.showInView(view, message: message, isDone: false)
     }
     
-    public static func showInView(view: UIView, isDone: Bool) {
+    open static func showInView(_ view: UIView, isDone: Bool) {
         CheckAndXHUD.sharedObject.showInView(view, message: nil, isDone: isDone)
     }
     
-    public static func showInView(view: UIView, message: String?, isDone: Bool) {
+    open static func showInView(_ view: UIView, message: String?, isDone: Bool) {
         CheckAndXHUD.sharedObject.showInView(view, message: message, isDone: isDone)
     }
     
-    private func showInView(view: UIView, message: String?, isDone: Bool) {
+    fileprivate func showInView(_ view: UIView, message: String?, isDone: Bool) {
         // Set size of done view
         let checkAndXViewWidth = min(view.frame.width, view.frame.height) / 2
         var originX: CGFloat, originY: CGFloat
-        if (UIDevice.currentDevice().systemVersion as NSString).floatValue >= 8.0 {
+        if (UIDevice.current.systemVersion as NSString).floatValue >= 8.0 {
             originX = (view.frame.width - checkAndXViewWidth) / 2
             originY = (view.frame.height - checkAndXViewWidth) / 2
         } else {
-            let isLandscape = UIDevice.currentDevice().orientation.isLandscape
+            let isLandscape = UIDevice.current.orientation.isLandscape
             originX = ((isLandscape ? view.frame.height : view.frame.width) - checkAndXViewWidth) / 2
             originY = ((isLandscape ? view.frame.width : view.frame.height) - checkAndXViewWidth) / 2
         }
-        let checkAndXViewFrame = CGRectMake(
-            originX,
-            originY,
-            checkAndXViewWidth,
-            checkAndXViewWidth)
+        let checkAndXViewFrame = CGRect(
+            x: originX,
+            y: originY,
+            width: checkAndXViewWidth,
+            height: checkAndXViewWidth)
         self.checkAndXView.layer.cornerRadius = 8
         self.checkAndXView.frame = checkAndXViewFrame
 
@@ -65,20 +65,20 @@ public class CheckAndXHUD: NSObject {
         // Start animation
         self.checkAndXView.alpha = 0
         view.addSubview(self.checkAndXView)
-        UIView.animateWithDuration(0.2, animations: { () -> Void in
+        UIView.animate(withDuration: 0.2, animations: { () -> Void in
             self.checkAndXView.alpha = 1
-        }) { (result: Bool) -> Void in
+        }, completion: { (result: Bool) -> Void in
             self.checkAndXView.drawCheck({ () -> Void in
-                let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(0.5 * Double(NSEC_PER_SEC)))
-                dispatch_after(delayTime, dispatch_get_main_queue()) {
-                    UIView.animateWithDuration(0.2, animations: { () -> Void in
+                let delayTime = DispatchTime.now() + Double(Int64(0.5 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+                DispatchQueue.main.asyncAfter(deadline: delayTime) {
+                    UIView.animate(withDuration: 0.2, animations: { () -> Void in
                         self.checkAndXView.alpha = 0
-                        }) { (result: Bool) -> Void in
+                        }, completion: { (result: Bool) -> Void in
                             self.checkAndXView.removeFromSuperview()
                             self.checkAndXView.clear()
-                    }
+                    }) 
                 }
             })
-        }
+        }) 
     }
 }
